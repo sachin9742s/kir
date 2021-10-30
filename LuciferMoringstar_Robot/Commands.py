ecd5635459014c485d5b1b64daa949cde1977b92
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-30 14:02:48 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import random
-from script import script
+import script
 from pyrogram import Client, filters
 from pyrogram import StopPropagation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
